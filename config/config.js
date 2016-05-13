f8d46b18a2a0a7505339faee2ecfdfe6b94ab7cd
georgesuveti <george.suveti@irian.ro>
2016-05-13 10:54:24 +0300

    order + deliver

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ var config = {
       name: 'express-postresql'
     },
     port: process.env.PORT || 3000,
-    db: 'postgres://postgres@localhost/rest1',
+    db: 'postgres://tdr:parola2016@mobile.itec.ligaac.ro/rest1',
     omitNull: true
   },
 
